67653bf6355f2e823c0905fb1546e180d7e23812
Liuklian Anna <liuklian.pn@ucu.edu.ua>
2026-05-07 22:52:38 +0300

    Build and validate house price prediction model
    Implemented RandomForestRegressor and evaluated performance using MAE on real estate data.

--- introduction_to_machine_learning/creating_model.py
+++
@@ -1,7 +0,0 @@
-from sklearn.tree import DecisionTreeRegressor
-
-# Define model. Specify a number for random_state to ensure same results each run
-melbourne_model = DecisionTreeRegressor(random_state=1)
-
-# Fit model
-melbourne_model.fit(X, y)
